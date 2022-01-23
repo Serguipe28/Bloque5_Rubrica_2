8343dbf34e57913a95ac59fc3752dd1f61763bf1
Sergio Guillen <serguipe28@gmail.com>
2022-01-23 22:30:54 +0100

    create

---
+++ src/main/java/com/serguipe/bloque5_rubrica_2/Bloque5Rubrica2CreatingAWebApplicationWithSpringBootDualApplication.java
@@ -0,0 +1,15 @@
+package com.serguipe.bloque5_rubrica_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.serguipe.bloque5_rubrica_2.*")
+public class Bloque5Rubrica2CreatingAWebApplicationWithSpringBootDualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bloque5Rubrica2CreatingAWebApplicationWithSpringBootDualApplication.class, args);
+	}
+
+}
